914b6da7465ce288426dfd38df1acae81e9de2f2
Akshat Mehta <contact@akshatmehta.xyz>
2021-06-13 18:29:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const client = new discord.Client({ disableMentions: 'everyone' });
 const keepAlive = require('./server.js');
 const { Player } = require('discord-player');
 
+const { MessageEmbed } = require('discord.js);
+
 client.on('guildCreate', function(guild) {
   let embed = new MessageEmbed()
   .setTitle('Trexy Joined A Guild.')
