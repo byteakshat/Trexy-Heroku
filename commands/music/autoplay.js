df3654e60e0cfe71075605eb43888ecd0be9a7bb
Akshat Mehta <contact@akshatmehta.xyz>
2021-06-21 22:06:50 +0530

    Autoplay command added 🙌

---
+++ commands/music/autoplay.js
@@ -0,0 +1,27 @@
+module.exports = {
+  name: "autoplay",
+  aliases: ['auto'],
+  category: 'Music',
+  utilisation: '{prefix}autoplay',
+  description: "Get all the song name which are in queue",
+  execute: (client, message, args) => {
+const queue = this.client.player.getQueue(message);
+
+		const voice = message.member.voice.channel;
+		if (!voice){
+			return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+		}
+
+		if(!queue){
+			return message.error(`${client.emotes.error} - There's no music playing`);
+		}
+
+		// Gets the current song
+		await this.client.player.setAutoPlay(message, !queue.autoPlay);
+        
+		// Send the embed in the current channel
+		message.channel.send(`${queue.autoPlay ? "ENABLED" : "DISABLED"}`);
+        
+	}
+
+}
