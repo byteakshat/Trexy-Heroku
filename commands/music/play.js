d9a9cfad4a5b148c658946be196da95edef571a7
Akshat Mehta <contact@akshatmehta.xyz>
2021-06-26 01:38:00 +0530

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,3 +1,6 @@
+
+const { Util } = require("discord-player");
+
 module.exports = {
     name: 'play',
     aliases: ['join', 'play', 'p'],
@@ -11,7 +14,11 @@ module.exports = {
 
         if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
 
-        client.player.play(message, args.join(" "), { firstResult: true });
-
+        const tracks = await Util.ytSearch(args.join(" "), {
+    user: message.author,
+    player: client.player
+}).catch(() => {});
+        
+        client.player.play(message, tracks[0].url, { firstResult: true });
     },
 }
