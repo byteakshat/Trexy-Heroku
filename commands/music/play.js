d37207e76e7da93a1dc2035c52a54796eba78ed2
Akshat Mehta <contact@akshatmehta.xyz>
2021-06-26 14:03:50 +0530

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,26 +1,16 @@
-
-const { Util } = require("discord-player");
-
 module.exports = {
     name: 'play',
-    aliases: ['join', 'play', 'p'],
+    aliases: ['p'],
     category: 'Music',
     utilisation: '{prefix}play [name/URL]',
 
-     execute: async (client, message, args) => {
+    execute(client, message, args) {
         if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
 
         if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
-         
-         if (Util.isURL(args.join(" "))) return client.player.play(message, args.join(" "), { firstResult: true });
-         
-        const tracks = await Util.ytSearch(args.join(" "), {
-    user: message.author,
-    player: client.player
-}).catch(() => {});
-        
-        client.player.play(message, tracks[0].url, { firstResult: true });
+
+        client.player.play(message, args.join(" "), { firstResult: true });
     },
-}
+};
